ab795dc5deee8389efa5370760e99b3a76e3a8f2
Jose Soto <jsoto7087@gmail.com>
2021-01-17 21:31:01 -0500

    commit tree 31b7134c75668544e9c76a790e7577cdef12ed84

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,22 @@
 #write your code here
 
 def countdown(enteger)
-    while enteger < 10
+    while enteger > 0
         puts "#{enteger} SECOND(S)!"
         enteger -= 1
     end
     return "HAPPY NEW YEAR!"
 end
 
+
+def countdown_with_sleep(seconds_to_midnight)
+    while seconds_to_midnight > 0
+        puts "#{seconds_to_midnight} SECOND(S) !"
+        sleep(1)
+        seconds_to_midnight -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
 
 # x = 1
 #while x < 10
