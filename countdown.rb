8fd12b3d17b3712fb7eb320fabaee85db2b474d0
Jose Soto <jsoto7087@gmail.com>
2021-01-17 21:15:36 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown(enteger)
+    while enteger < 10
+        puts "#{enteger} SECOND(S)!"
+        enteger -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
+
+
+# x = 1
+#while x < 10
+ # puts "#{x} is less than 10"
+  #x += 1
+#end 
